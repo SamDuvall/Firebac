d8f2d236b83788980efe38f48c104d514bb7e577
Sam Duvall <SamDuvall@gmail.com>
2014-01-11 18:07:04 -0500

    CRUD kinda works

--- js/model/organization.js
+++ js/model/organization.js
@@ -1,6 +1,29 @@
+var Member = Backbone.Model.extend({
+  doSomething: function() {
+    console.log(this);
+  }
+});
+
+Member.Collection = Backbone.Firebase.Collection.extend({
+  model: Member
+});
+
 var Organization = Backbone.Model.extend({
   doSomething: function() {
     console.log(this);
+  }
+},{
+  Member: Member
+});
+
+Object.defineProperty(Organization.prototype, 'members', {
+  enumerable: true,
+  get: function() {
+    if (this._members) return this._members;
+    this._members = new Member.Collection(null, {
+      firebase: "https://rbac.firebaseio.com/organizations/" + this.id + "/members" 
+    });
+    return this._members;
   }
 });
 
--- js/view/organization/crud.js
+++ js/view/organization/crud.js
@@ -1,11 +1,60 @@
 var Organization = require('../../model/organization');
 
+var MemberView = Backbone.Marionette.CollectionView.extend({
+  tagName: 'li',
+  className: 'member',
+
+  events: {
+    'click .remove-member': 'removeMember'
+  },
+
+  render: function() {
+    this.$el.html(this.model.id);
+    this.$el.append('<button type="button" class="remove-member">x</member>');
+    return this;
+  },
+
+  removeMember: function() {
+    this.model.destroy();
+  }
+});
+
+var MembersView = Backbone.Marionette.CollectionView.extend({
+  tagName: 'ul',
+  className: 'members',
+  itemView: MemberView
+});
+
 var ItemView = Backbone.Marionette.CollectionView.extend({
   tagName: 'li',
   className: 'organization',
+
+  events: {
+    'click .remove-organization': 'removeOrganization',
+    'click .add-member': 'addMember'
+  },
+
   render: function() {
-    this.$el.html(this.model.get('id'));
+    this.$el.html(this.model.id);
+    this.$el.append('<button type="button" class="remove-organization">x</member>');
+
+    var list = new MembersView({
+      collection: this.model.members
+    });
+    this.$el.append(list.render().el);
+
+    this.$el.append('<button type="button" class="add-member">Add Member</member>');
     return this;
+  },
+
+  removeOrganization: function() {
+    this.model.destroy();
+  },
+
+  addMember: function() {
+    this.model.members.create({
+      email: 'email@test.com'
+    })
   }
 });
 
